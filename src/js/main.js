7763296fd5fbd14be19d04f74652b5f54591cad5
Shane T <st83440@gmail.com>
2024-07-11 14:15:15 -0600

    add comments

--- src/js/main.js
+++ src/js/main.js
@@ -10,10 +10,14 @@ function parkInfoTemplate(info) {
   </p>`;
 }
 
+// insert data into disclaimer section
 const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = parkData.url;
-document.querySelector("head > title").textContent = parkData.fullName;
 disclaimer.innerHTML = parkData.fullName;
+// update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+document.querySelector("head > title").textContent = parkData.fullName;
+// set the banner image
 document.querySelector(".hero-banner > img").src = parkData.images[0].url;
+// use the template function above to set the rest of the park specific info in the header
 document.querySelector(".hero-banner__content").innerHTML =
   parkInfoTemplate(parkData);
