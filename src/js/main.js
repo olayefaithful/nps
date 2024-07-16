3f96b054e2592faac780deb044145994d3efdbb1
Shane T <st83440@gmail.com>
2024-07-16 14:51:16 -0600

    more refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,7 @@
 import { getParkData, parkInfoLinks } from "./parkService.mjs";
-import {
-  parkInfoTemplate,
-  mediaCardTemplate,
-  footerTemplate
-} from "./templates.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 const parkData = getParkData();
-
-function setHeaderInfo(data) {
-  // insert data into disclaimer section
-  const disclaimer = document.querySelector(".disclaimer > a");
-  disclaimer.href = data.url;
-  disclaimer.innerHTML = data.fullName;
-  // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
-  document.querySelector("head > title").textContent = data.fullName;
-  // set the banner image
-  document.querySelector(".hero-banner > img").src = data.images[0].url;
-  // use the template function above to set the rest of the park specific info in the header
-  document.querySelector(".hero-banner__content").innerHTML =
-    parkInfoTemplate(data);
-}
 
 function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
@@ -28,16 +11,12 @@ function setParkIntro(data) {
 
 function setParkInfoLinks(data) {
   const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
   const html = data.map(mediaCardTemplate);
-  infoEl.innerHTML = html.join("");
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-function setFooter(data) {
-  const footerEl = document.querySelector("#park-footer");
-  footerEl.innerHTML = footerTemplate(data);
-}
-
-setHeaderInfo(parkData);
+setHeaderFooter(parkData);
 setParkIntro(parkData);
 setParkInfoLinks(parkInfoLinks);
-setFooter(parkData);
