537c83ae700e0e8498e9acb3e7f0b15e9aa02e8f
Shane T <st83440@gmail.com>
2024-07-18 10:24:37 -0600

    initial unit3 content

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -37,3 +37,33 @@ export function footerTemplate(info) {
   </section>
     `;
 }
+
+export function alertTemplate(alert) {
+  let alertType = "";
+  // most of the alerts are one word and line up with the icons nicely. "Park Closure" is the exception
+  switch (alert.category) {
+    case "Park Closure":
+      alertType = "closure";
+      break;
+    default:
+      alertType = alert.category.toLowerCase();
+  }
+  return `<li class="alert">
+  <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use></svg>
+  <div>
+    <h3 class="alert-${alertType}">${alert.title}</h3>
+    <p>${alert.description}</p>
+  </div></li>`;
+}
+
+export function visitorCenterTemplate(center) {
+  return `<li class="visitor-center">
+  <h4>${center.name}</h4>
+  <p>${center.description}</p>
+  <p>${center.directionsInfo}</p>
+  </li>`;
+}
+
+export function activityListTemplate(activities) {
+  return activities.map((activity) => `<li>${activity.name}</li>`).join("");
+}
