0a502e8645790a2d57cb405f6f52687f3f3b46b9
Shane T <st83440@gmail.com>
2024-09-20 14:36:04 -0600

    finish 4

--- src/js/main.js
+++ src/js/main.js
@@ -24,4 +24,27 @@ async function init() {
   setParkInfoLinks(links);
 }
 
+function enableNavigation() {
+  const menuButton = document.querySelector("#global-nav-toggle");
+  const subMenuToggles = document.querySelectorAll(
+    ".global-nav__split-button__toggle"
+  );
+  // when the main menu button is clicked:
+  menuButton.addEventListener("click", (ev) => {
+    // toggle the show class on the global-nav
+    document.querySelector(".global-nav").classList.toggle("show");
+    // check to see if we just opened or closed the menu
+    if (document.querySelector(".global-nav").classList.contains("show")) {
+      // if we opened it then set the aria-expanded attribute to true
+      menuButton.setAttribute("aria-expanded", true);
+    } else {
+      // if we closed it then set the aria-expanded attribute to false
+      menuButton.setAttribute("aria-expanded", false);
+    }
+
+    console.log("toggle");
+  });
+}
+
 init();
+enableNavigation();
