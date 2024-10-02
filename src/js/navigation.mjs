645e3cfbe3b7c866cca900a4e72a276d3e92e7ee
Shane T <st83440@gmail.com>
2024-10-02 13:39:09 -0600

    init

--- src/js/navigation.mjs
+++ src/js/navigation.mjs
@@ -1,13 +1,18 @@
 function mainMenuHandler(ev) {
+  let target = ev.target;
   // toggle the show class on the global-nav
   document.querySelector(".global-nav").classList.toggle("show");
+  // check to see if ev.target is the button or something inside the button
+  if (target.tagName != "BUTTON") {
+    target = target.closest("button");
+  }
   // check to see if we just opened or closed the menu
   if (document.querySelector(".global-nav").classList.contains("show")) {
     // if we opened it then set the aria-expanded attribute to true
-    ev.target.setAttribute("aria-expanded", true);
+    target.setAttribute("aria-expanded", true);
   } else {
     // if we closed it then set the aria-expanded attribute to false
-    ev.target.setAttribute("aria-expanded", false);
+    target.setAttribute("aria-expanded", false);
   }
 
   console.log("toggle");
