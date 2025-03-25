077135a58c7640719017c76fef947b21c221a5c2
Shane T <st83440@gmail.com>
2025-03-25 10:06:18 -0600

    update vite to v6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { resolve } from "path";
+import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: "src/",
