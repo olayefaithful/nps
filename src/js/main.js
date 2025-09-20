7530e2ad648eecb789d6678ea84fd5ee057a6d4a
Faithful Olaye <olayefaithful@gmail.com>
2025-09-20 01:25:28 -0600

    Completed NPS park page with dynamic content

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
-import { getParkData } from "./parkService.mjs";
+
+import { getParkData } from './parkService.mjs';
 
 const parkData = getParkData();
+const disclaimerLink = document.querySelector('.disclaimer > a');
+disclaimerLink.href = parkData.url;
+disclaimerLink.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImg = document.querySelector('.hero-banner img');
+heroImg.src = parkData.images[0].url;
+heroImg.alt = parkData.images[0].altText;
+
+function parkInfoTemplate(info) {
+  return `
+    <a href="${info.url}" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>
+  `;
+}
+
+const heroText = document.querySelector('.hero-banner__content');
+heroText.innerHTML = parkInfoTemplate(parkData);
